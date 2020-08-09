91b6de9cddbdc940044b7411c593455f10506335
Poss111 <dpatposs@msn.com>
2020-08-09 18:55:51 -0500

    Adding wildcard route for EntryComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import {EntryComponent} from "./entry/entry.component";
 import {RandomPickComponent} from "./components/random-pick/random-pick.component"; // CLI imports router
 
-const routes: Routes = [{path: 'entry', component: EntryComponent},
-  {path: 'lane-rando', component: RandomPickComponent},
-  { path: '',   redirectTo: '/entry', pathMatch: 'full'}]; // sets up routes constant where you define your routes
+const routes: Routes = [
+  { path: 'entry', component: EntryComponent },
+  { path: 'lane-rando', component: RandomPickComponent },
+  { path: '',   redirectTo: '/entry', pathMatch: 'full'},
+  { path: '**', component: EntryComponent }]; // sets up routes constant where you define your routes
 
 // configures NgModule imports and exports
 @NgModule({

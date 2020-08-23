4a39b96dd345301bfe90ff765b23392cc6d928eb
Daniel Poss <dpatposs@msn.com>
2020-08-23 00:30:30 -0500

    Moving script to Cloudformation template - Daniel

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash
-yum -y install nodejs
+apt-get -y install nodejs
 
 apt-get update
-apt-get install nginx
+apt-get -y install nginx
 cp /home/ubuntu/LaneRando/serverConfig/server.conf /etc/nginx/conf.d/server1.conf
 service nginx start

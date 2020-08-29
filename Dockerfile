bcd54829290a409ef404d56812e3f5afa83f3d23
Daniel Poss <dpatposs@msn.com>
2020-08-29 12:44:51 -0500

    Updating docker file to have step to resolve 404 issue - Daniel

--- Dockerfile
+++ Dockerfile
@@ -9,3 +9,4 @@ RUN npm run build --prod
 
 FROM nginx:1.17.1-alpine
 COPY --from=build-step /application/dist/LaneRando /usr/share/nginx/html
+COPY --from=build-step /application/serverConfig/default.conf /etc/nginx/conf.d/default.conf
--- setup.sh
+++ setup.sh
@@ -1,7 +1,2 @@
-curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash
-apt-get -y install nodejs
-
-apt-get update
-apt-get -y install nginx
-cp /home/ubuntu/LaneRando/serverConfig/server.conf /etc/nginx/conf.d/server1.conf
-service nginx start
+docker build -t poss11111/lane-rando-ui .
+docker images

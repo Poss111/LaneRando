d167a537fda0f40791de12454a5c2ec63333f881
Daniel Poss <dpatposs@msn.com>
2020-08-30 20:34:54 -0500

    Changing path names for pages - Daniel

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 docker build -t poss11111/lane-rando-ui .
 docker images
+docker run -d -it -p 80:80/tcp --name lane-rando-ui poss11111/lane-rando-ui:latest
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,8 @@ import {EntryComponent} from './components/entry/entry.component';
 import {RandomPickComponent} from './components/random-pick/random-pick.component';
 
 export const routes: Routes = [
-  { path: 'entry', component: EntryComponent },
-  { path: 'lane-rando', component: RandomPickComponent },
-  { path: '',   redirectTo: '/entry', pathMatch: 'full'},
+  { path: '', component: EntryComponent },
+  { path: 'randomize', component: RandomPickComponent },
   { path: '**', component: EntryComponent }];
 
 @NgModule({
